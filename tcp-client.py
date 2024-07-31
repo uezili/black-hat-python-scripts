a24f2f5beaaf79431464e0b46ba99d91e900199e
Wesley <weslmaciel@gmail.com>
2024-07-30 21:30:33 -0300

    Server and client tcp with socket libery.

---
+++ tcp-client.py
@@ -0,0 +1,17 @@
+import socket
+target_host = "0.0.0.0"
+target_port = 9998
+
+#create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#connet the client 
+client.connect((target_host, target_port))
+
+#send some data
+client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+response = client.recv(4096)
+
+print(response.decode())
+client.close()
